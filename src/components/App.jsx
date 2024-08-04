d35bd66cb52aa610b2708d1194c9603938f8a944
Nio <nikris@mail.ru>
2024-08-04 14:58:21 +0300

    Added aditional functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,17 @@ import Loader from './Loader'
 import Error from './Error'
 import StartScreen from './StartScreen'
 import Question from './Question'
+import NextButton from './NextButton'
+import Progress from './Progress'
+import FinishScreen from './FinishScreen'
 
 const initialState = {
   questions: [],
   status: 'loading',
   index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0,
 }
 
 function reducer(state, action) {
@@ -23,15 +29,50 @@ function reducer(state, action) {
     case 'start':
       return { ...state, status: 'active' }
 
+    case 'newAnswer': {
+      const question = state.questions.at(state.index)
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      }
+    }
+
+    case 'nextQuestion':
+      return { ...state, index: state.index + 1, answer: null }
+
+    case 'finish':
+      return {
+        ...state,
+        status: 'finished',
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      }
+
+    case 'restart':
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: 'ready',
+      }
+
     default:
       throw new Error('Action unknown')
   }
 }
 
 function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState)
+  const [{ questions, status, index, answer, points, highscore }, dispatch] =
+    useReducer(reducer, initialState)
 
   const numQuestions = questions.length
+  const maxPossiblePoints = questions.reduce(
+    (acc, next) => acc + next.points,
+    0
+  )
 
   useEffect(() => {
     fetch('http://localhost:8000/questions')
@@ -50,7 +91,36 @@ function App() {
         {status === 'ready' && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === 'active' && <Question />}
+        {status === 'active' && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              index={index}
+              numQuestions={numQuestions}
+            />
+          </>
+        )}
+        {status === 'finished' && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+            dispatch={dispatch}
+          />
+        )}
       </Main>
     </div>
   )
